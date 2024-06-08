3446e7d68629a6ed16bc2946b9c0adfbbb9c383e
menglight <menglight02@gmail.com>
2024-06-08 19:03:17 +0800

    change upload

--- code.c
+++ code.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
-
 int main()
 {
-
-
-
-
-	printf("code1.\n");
-
-	printf("code1.\n");
-	printf("code1.\n");
-	printf("code1.\n");
-	printf("code1.\n");
-	printf("code1.\n");
+	printf("hello world");
 	return 0;
 }
